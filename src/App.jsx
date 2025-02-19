c76cf0d139967cc280b56c4b56801295cc84bbaf
hossam6854 <150062047+hossam6854@users.noreply.github.com>
2025-02-19 14:22:26 +0200

    updates1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, HashRouter as Router } from "react-router-dom";
 import "./App.css";
 import AddHabit from "./Components/AddHabit";
 import { ApiProvider } from "./Components/contextapi";
